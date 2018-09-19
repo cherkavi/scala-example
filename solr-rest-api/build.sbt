b5a79eef1690f4c348ef80866cc075f3e70e2190
Vitalii <technik7job@gmail.com>
2018-09-19 22:20:44 +0200

    solr check version via Http.GET Json request

--- solr-rest-api/build.sbt
+++ solr-rest-api/build.sbt
@@ -6,3 +6,6 @@ scalaVersion := "2.11.12"
 
 // https://mvnrepository.com/artifact/org.apache.httpcomponents/httpclient
 libraryDependencies += "org.apache.httpcomponents" % "httpclient" % "4.5.4"
+
+// https://mvnrepository.com/artifact/com.google.code.gson/gson
+libraryDependencies += "com.google.code.gson" % "gson" % "2.8.5"
--- solr-rest-api/src/main/scala/SolrRestApi.scala
+++ solr-rest-api/src/main/scala/SolrRestApi.scala
@@ -1,13 +1,23 @@
 package com.cherkashyn.vitalii.solr_api
 
+import scala.collection.immutable.HashMap
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
 
 /**
   */
 object SolrRestApi {
+  private val URL = "http://localhost:8983"
 
-  def getSolrVersion(): String = {
-    "version"
+  def getSolrVersion(): Any = {
+    import scala.io.Source
+    val html = Source.fromURL(s"${URL}/solr/admin/info/system?wt=json")
+    val json= new JsonParser().parse(html.mkString).getAsJsonObject
+    json.get("lucene").asInstanceOf[JsonObject].get("solr-spec-version")
   }
+
+
 
   def main(args: Array[String]): Unit ={
     println(getSolrVersion())
